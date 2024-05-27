016ebfdd974e9e81b689ec75176314ce86bc315f
apachertr180navneeet <navneetgehlot03061993@gmail.com>
2024-05-27 11:58:45 +0530

    UI changes

--- resources/views/admin/layouts/app.blade.php
+++ resources/views/admin/layouts/app.blade.php
@@ -16,6 +16,7 @@
         <link rel="stylesheet" href="{{asset('assets/admin/vendor/fonts/boxicons.css')}}" />
         <link rel="stylesheet" href="{{asset('assets/admin/vendor/css/core.css')}}" class="template-customizer-core-css" />
         <link rel="stylesheet" href="{{asset('assets/admin/vendor/css/theme-default.css')}}" class="template-customizer-theme-css" />
+        <link rel="stylesheet" href="{{asset('assets/admin/css/custom.css')}}" />
         <link rel="stylesheet" href="{{asset('assets/admin/css/demo.css')}}" />
         <link rel="stylesheet" href="{{asset('assets/admin/css/bootstrapDataTable.css')}}" />
         <link rel="stylesheet" href="{{asset('assets/admin/vendor/libs/perfect-scrollbar/perfect-scrollbar.css')}}" />
